88a8a7c5b85ab8aeed1e3ae80ee45ae9cc95e044
8-bitz <46509022+8-bitz@users.noreply.github.com>
2019-02-09 18:26:40 -0500

    Using reversed ascii characters for better coverage

--- asciiart_colour.py
+++ asciiart_colour.py
@@ -25,6 +25,12 @@ def generateFrameConversionData(imgPath):
 		"32" : "f","31" : "t","30" : "/","29" : "\\","28" : "|","27" : "(","26" : ")","25" : "1","24" : "{","23" : "}","22" : "[","21" : "]","20" : "?","19" : "-","18" : "_","17" : "+","16" : "~","15" : "<","14" : ">",
 		"13" : "i","12" : "!","11" : "l","10" : "I","9" : ";","8" : ":","7" : ",","6" : "\"","5" : "^","4" : "`","3" : "'","2" : ".","1" :  " "	,"0" :  " "
         }
+    greyscaleCharsRev = {
+        "0" : "$","1" : "@","2" : "B","3" : "%","4" : "8","5" : "&","6" : "W","7" : "M","8" : "#","9" : "*","10" : "o","11" : "a","12" : "h","13" : "k","14" : "b","15" : "d","16" : "p","17" : "q","18" : "w",
+        "19" : "m","20" : "Z","21" : "O","22" : "0","23" : "Q","24" : "L","25" : "C","26" : "J","27" : "U","28" : "Y","29" : "X","30" : "z","31" : "c","32" : "v","33" : "u","34" : "n","35" : "x","36" : "r","37" : "j",
+        "38" : "f","39" : "t","40" : "/","41" : "\\","42" : "|","43" : "(","44" : ")","45" : "1","46" : "{","47" : "}","48" : "[","49" : "]","50" : "?","51" : "-","52" : "_","53" : "+","54" : "~","55" : "<","56" : ">",
+        "57" : "i","58" : "!","59" : "l","60" : "I","61" : ";","62" : ":","63" : ",","64" : "\"","65" : "^","66" : "`","67" : "'","68" : ".","69" :  " "	,"70" :  " "
+        }
     img = Image.open(imgPath)
     img = img.convert("RGB")
     gImg = img.convert("L")
@@ -39,7 +45,7 @@ def generateFrameConversionData(imgPath):
             pixColour = img.getpixel((x,y))
             #pix = brightnessAdjust(pix, brightnessAdjustment)					#adjust the brightness of the individual pixel
             pix = int(pix / divisor)											#assign pixel shade to an ascii character
-            asciiChar = 	greyscaleChars[str(pix)]						#Create the output string
+            asciiChar = 	greyscaleCharsRev[str(pix)]						#Create the output string
             imgData.append((asciiChar,pixColour))
         asciiChar = "\n"
         pixColour = (0,0,0)
